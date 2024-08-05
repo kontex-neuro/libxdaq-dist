97578e40422054d594ac6462f799e0d44ef3a1ae
stonelin <jason840708@gmail.com>
2024-08-05 10:51:31 +0800

    Create conan package

--- conanfile.py
+++ conanfile.py
@@ -1,10 +1,17 @@
+from pathlib import Path
 from conan import ConanFile
-from conan.tools.cmake import CMakeToolchain
+from conan.tools.files import copy
 
 
 class libxdaq(ConanFile):
+    name = "libxdaq"
+    version = "0.1.0"
     settings = "os", "compiler", "build_type", "arch"
-    generators = "CMakeDeps", "VirtualRunEnv"
+    build_policy = "missing"
+
+    def build_requirements(self):
+        self.requires("catch2/3.5.0")
+        self.tool_requires("cmake/[>=3.25.0 <3.30.0]")
 
     def requirements(self):
         self.requires("boost/1.81.0")
@@ -12,14 +19,42 @@ class libxdaq(ConanFile):
         self.requires("fmt/10.2.1")
         self.requires("nlohmann_json/3.11.3")
         self.requires("spdlog/1.13.0")
-        self.requires("catch2/3.5.0")
 
     def configure(self):
+        # Enable required Boost modules
+        self.options["boost/*"].with_atomic = True
+        self.options["boost/*"].with_system = True
+        self.options["boost/*"].with_filesystem = True
+        self.options["boost/*"].with_program_options = True
+        # Configure Boost options for GitHub Actions build
         self.options["boost/*"].with_stacktrace_backtrace = False
-        self.options["boost/*"].without_locale = True
         self.options["boost/*"].without_stacktrace = True
+        self.options["boost/*"].without_locale = True
+        # List of Boost modules to disable for faster build speed
+        disable_for_build_speed = (
+            "charconv", "chrono", "cobalt", "container", "context", "contract", "coroutine",
+            "date_time", "exception", "fiber", "graph", "graph_parallel", "iostreams", "json",
+            "log", "math", "mpi", "nowide", "python", "random", "regex", "serialization", "test",
+            "thread", "timer", "type_erasure", "url", "wave"
+        )
+        for opt in disable_for_build_speed:
+            setattr(self.options["boost/*"], f"without_{opt}", True)
 
-    def generate(self):
-        tc = CMakeToolchain(self)
-        # tc.user_presets_path = False
-        tc.generate()
+    def layout(self):
+        _os = str(self.settings.os).lower()
+        _arch = str(self.settings.arch).lower()
+        self.folders.build = Path("libxdaq") / _os / _arch
+        self.folders.source = self.folders.build
+        self.folders.generators = Path("build")
+
+    def package(self):
+        local_bin_folder = Path(self.build_folder, "bin")
+        local_include_folder = Path(self.build_folder, "include")
+        local_lib_folder = Path(self.build_folder, "lib")
+        copy(self, "*", local_bin_folder, Path(self.package_folder, "bin"))
+        copy(self, "*", local_include_folder, Path(self.package_folder, "include"))
+        copy(self, "*", local_lib_folder, Path(self.package_folder, "lib"))
+
+    def package_info(self):
+        self.cpp_info.set_property("cmake_find_mode", "none")
+        self.cpp_info.builddirs.append(str(Path("lib", "cmake", "xdaq")))
---
+++ test_package/conanfile.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
+from conan.tools.build import can_run
+
+
+class libxdaq(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+
+    def build_requirements(self):
+        self.tool_requires("cmake/[>=3.25.0 <3.30.0]")
+
+    def requirements(self):
+        self.requires("catch2/3.5.0")
+        self.requires("fmt/10.2.1")
+        self.requires("nlohmann_json/3.11.3")
+        self.requires(self.tested_reference_str)
+
+    def layout(self):
+        cmake_layout(self)
+
+    def generate(self):
+        deps = CMakeDeps(self)
+        deps.generate()
+        tc = CMakeToolchain(self)
+        tc.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def test(self):
+        if can_run(self):
+            cmd = Path(self.cpp.build.bindir, "test_plugin")
+            self.run(str(cmd.absolute()), env="conanrun")
