8e4e4c50439294f22bc9f602a9254b4d17b8dff1
stonelin <jason840708@gmail.com>
2025-02-28 14:23:17 +0530

    0.2.4 Release
    - Improved stability
    - Firmware upgrade required

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,7 @@ from conan.tools.files import get, copy
 
 class libxdaq(ConanFile):
     name = "libxdaq"
-    version = "0.2.2"
+    version = "0.2.4"
     settings = "os", "compiler", "build_type", "arch"
     build_policy = "missing"
 
