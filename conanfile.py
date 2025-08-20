b75056810a52d7e129e11eba796e8030254dbcaa
StoneLin <jason840708@gmail.com>
2025-08-20 22:14:19 +0800

    0.4.1 Release
    Check if the device timestamp is supported in the current firmware

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,7 @@ from conan.tools.files import get, copy
 
 class libxdaq(ConanFile):
     name = "libxdaq"
-    version = "0.4.0"
+    version = "0.4.1"
     settings = "os", "compiler", "build_type", "arch"
     build_policy = "missing"
 
