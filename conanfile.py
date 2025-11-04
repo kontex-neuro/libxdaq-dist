955b93caf2c8ac247e1100f7c7fd73007845383a
stonelin <jason840708@gmail.com>
2025-11-04 17:07:48 +0800

    0.5.2 Release
    - Add support for device detection using newly released Linux driver

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,7 @@ from conan.tools.files import get, copy
 
 class libxdaq(ConanFile):
     name = "libxdaq"
-    version = "0.5.1"
+    version = "0.5.2"
     settings = "os", "compiler", "build_type", "arch"
     build_policy = "missing"
 
